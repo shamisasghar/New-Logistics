8b1a8983d404efa2e1f947a30b54060032d260c9
shamis <shamis.asghar@outlook.com>
2018-01-21 00:28:35 +0500

    Home fragment

--- app/src/main/java/com/appinspire/dailybudget/fragments/HomeFragment.java
+++ app/src/main/java/com/appinspire/dailybudget/fragments/HomeFragment.java
@@ -8,11 +8,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.appinspire.dailybudget.FrameActivity;
 import com.appinspire.dailybudget.R;
+import com.appinspire.dailybudget.models.AssignedTime;
+import com.appinspire.dailybudget.models.Time;
 import com.appinspire.dailybudget.toolbox.ToolbarListener;
 import com.appinspire.dailybudget.utils.ActivityUtils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import iammert.com.expandablelib.ExpandableLayout;
+import iammert.com.expandablelib.Section;
 
 /**
  * Created by Bilal Rashid on 10/10/2017.
@@ -35,13 +44,34 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
         }
     }
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view= inflater.inflate(R.layout.fragment_home, container, false);
+
+        ExpandableLayout sectionLinearLayout = (ExpandableLayout) view.findViewById(R.id.layout_expandable);
+
+        sectionLinearLayout.setRenderer(new ExpandableLayout.Renderer<AssignedTime,Time>() {
+            @Override
+            public void renderParent(View view, AssignedTime model, boolean isExpanded, int parentPosition) {
+                ((TextView) view.findViewById(R.id.tvParent)).setText(model.name);
+                view.findViewById(R.id.arrow).setBackgroundResource(isExpanded ? R.drawable.up_arrow : R.drawable.up_arrow);
+            }
+
+            @Override
+            public void renderChild(View view, Time model, int parentPosition, int childPosition) {
+                ((TextView) view.findViewById(R.id.tvChild)).setText(model.name);
+
+            }
+        });
+
+        sectionLinearLayout.addSection(getsection());
+        sectionLinearLayout.addSection(getsection());
+        return view;
+
     }
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mHolder = new ViewHolder(view);
-        mHolder.button.setOnClickListener(this);
+    //    mHolder.button.setOnClickListener(this);
 //        Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.toolbar);
 //        toolbar.setOnClickListener(this);
     }
@@ -56,9 +86,26 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
 
         Button button;
         public ViewHolder(View view) {
-            button = (Button) view.findViewById(R.id.button);
+         //   button = (Button) view.findViewById(R.id.button);
 
         }
 
     }
+    public Section<AssignedTime,Time> getsection() {
+        Section<AssignedTime, Time> section = new Section<>();
+        AssignedTime phoneCategory=new AssignedTime("Phone");
+        // PhoneCategory phoneCategory1=new PhoneCategory("sssss");
+        List<Time> list=new ArrayList<Time>();
+        {
+            for (int i=0;i<=5;i++)
+
+                list.add(new Time("21313"+i));
+            section.parent=phoneCategory;
+            //  section.parent=phoneCategory1;
+            section.children.addAll(list);
+
+        }
+        return section;
+    }
+
 }
---
+++ app/src/main/java/com/appinspire/dailybudget/models/AssignedTime.java
@@ -0,0 +1,13 @@
+package com.appinspire.dailybudget.models;
+
+/**
+ * Created by Metis on 20-Jan-18.
+ */
+
+public class AssignedTime {
+    public String name;
+
+    public AssignedTime(String name) {
+        this.name = name;
+    }
+}
