b121a45b67bd209991eed317c8654faad37dfc51
shamis <shamis.asghar@outlook.com>
2018-01-22 14:54:29 +0500

    logo inserts

--- app/src/main/java/com/hypernymbiz/logistics/fragments/HomeFragment.java
+++ app/src/main/java/com/hypernymbiz/logistics/fragments/HomeFragment.java
@@ -2,7 +2,12 @@ package com.hypernymbiz.logistics.fragments;
 
 
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,6 +15,12 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
 import com.hypernymbiz.logistics.FrameActivity;
 import com.hypernymbiz.logistics.R;
 import com.hypernymbiz.logistics.models.AssignedTime;
@@ -27,8 +38,13 @@ import iammert.com.expandablelib.Section;
  * Created by Bilal Rashid on 10/10/2017.
  */
 
-public class HomeFragment extends Fragment implements View.OnClickListener{
+public class HomeFragment extends Fragment implements View.OnClickListener,OnMapReadyCallback {
     private ViewHolder mHolder;
+    private GoogleMap googleMap;
+    Context fContext;
+    LocationManager locationManager;
+    MapView mMapView;
+    LatLng pos;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,6 +61,64 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
     }
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view= inflater.inflate(R.layout.fragment_home, container, false);
+
+        mMapView = (MapView) view.findViewById(R.id.mapView);
+
+        locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+        if (ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getActivity().getApplicationContext(), android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+
+        } else {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{
+                    android.Manifest.permission.ACCESS_FINE_LOCATION,
+                    android.Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+        }
+        if (locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+
+
+                    Location l = (Location) location;
+                    pos = new LatLng(l.getLatitude(), l.getLongitude());
+                    if (l!=null){
+                        googleMap.setMyLocationEnabled(true);
+                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(pos,18.4f));
+                        googleMap.setTrafficEnabled(true);
+                    }
+
+
+//                 googleMap.addMarker(new MarkerOptions().position(pos).title("Driver Location").icon(BitmapDescriptorFactory.fromResource(R.drawable.maptruck)));
+
+                }
+
+                @Override
+                public void onStatusChanged(String s, int i, Bundle bundle) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String s) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String s) {
+
+                }
+            });
+
+        }
+        mMapView.onCreate(savedInstanceState);
+        mMapView.onResume();
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        mMapView.getMapAsync(this);
+
 
         ExpandableLayout sectionLinearLayout = (ExpandableLayout) view.findViewById(R.id.layout_expandable);
 
@@ -83,6 +157,12 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
 
     }
 
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        this.googleMap=googleMap;
+
+    }
+
     public static class ViewHolder {
 
         Button button;
@@ -94,7 +174,7 @@ public class HomeFragment extends Fragment implements View.OnClickListener{
     }
     public Section<AssignedTime,Time> getsection() {
         Section<AssignedTime, Time> section = new Section<>();
-        AssignedTime phoneCategory=new AssignedTime("Phone");
+        AssignedTime phoneCategory=new AssignedTime("Assigned Time");
         List<Time> list=new ArrayList<Time>();
         {
             for (int i=0;i<=5;i++)
